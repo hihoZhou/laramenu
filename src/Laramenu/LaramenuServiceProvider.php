64ac8e3b514656b2034e6de67433d2e06ef77d48
hihoZhou <hihozhou@gmail.com>
2016-11-14 15:21:13 +0800

    first commit

---
+++ src/Laramenu/LaramenuServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace hihozhou\Laramenu;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * LaramenuServiceProvider.php.
+ *
+ *
+ * @author    hihozhou <hihozhou.github.io>
+ * @email     hihozhou@gmail.com
+ * Time: 16-11-14 下午2:16
+ */
+class LaramenuServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        //
+    }
+}
